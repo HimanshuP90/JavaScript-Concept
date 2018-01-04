3799e02b9e3c37bdd73d6fa6814c9366a8bc3b06
HimanshuP90 <himanshup895@gmail.com>
2018-01-04 11:19:22 +0530

    Timers concepts in JS

--- main.js
+++ main.js
@@ -13,36 +13,51 @@
 
 // })();
 
-function generate_table() {
+// function generate_table() {
 
-	//Get the refrence of the body
-	var body = document.getElementsByTagName('body')[0];
+// 	//Get the refrence of the body
+// 	var body = document.getElementsByTagName('body')[0];
 
-	//Creates a <table> element and  a <tbody> element
-	var tbl = document.createElement("table");
-	var tblBody = document.createElement("tbody");
+// 	//Creates a <table> element and  a <tbody> element
+// 	var tbl = document.createElement("table");
+// 	var tblBody = document.createElement("tbody");
 
-	//Creating  all cells
-	for (var i = 0; i < 1; i++) {
+// 	//Creating  all cells
+// 	for (var i = 0; i < 1; i++) {
 
-		//create a table row
-		var row = document.createElement("tr");
+// 		//create a table row
+// 		var row = document.createElement("tr");
 
-		for (var j = 0; j < 4; j++) {
-			var cell = document.createElement("td");
-			var cellText = document.createTextNode(j);
-			cell.appendChild(cellText);
-			row.appendChild(cell);
-		}
-		tblBody.appendChild(row);
-	}
+// 		for (var j = 0; j < 4; j++) {
+// 			var cell = document.createElement("td");
+// 			var cellText = document.createTextNode(j);
+// 			cell.appendChild(cellText);
+// 			row.appendChild(cell);
+// 		}
+// 		tblBody.appendChild(row);
+// 	}
 
-	//put the <tbody> in <table>
-	tbl.appendChild(tblBody);
-	//append <table> into <body>
-	body.appendChild(tbl);
-	tbl.setAttribute("border", "2");
-	tbl.style.backgroundColor = "cyan";
+// 	//put the <tbody> in <table>
+// 	tbl.appendChild(tblBody);
+// 	//append <table> into <body>
+// 	body.appendChild(tbl);
+// 	tbl.setAttribute("border", "2");
+// 	tbl.style.backgroundColor = "cyan";
 
 
-}
+// }
+
+(function(){
+
+	var mainHeading = document.getElementById("main-heading");
+	var header = document.getElementsByClassName("header")[0];
+
+	mainHeading.addEventListener('click', function(){
+		console.log("I was clicked ..!!");
+	});
+
+	header.addEventListener('click', function(){
+		console.log("Parent element listener was clicked ..!!");
+	}, true);
+
+})();
