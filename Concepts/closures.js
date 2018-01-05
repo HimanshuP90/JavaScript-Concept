283fac1c940b2e55176c0354640c84b062e40a24
HimanshuP90 <himanshup895@gmail.com>
2018-01-05 11:14:57 +0530

    added closures definition

--- Concepts/closures.js
+++ Concepts/closures.js
@@ -1,22 +1,27 @@
-// (function(){
+(function(){
 
-// 	function youSayGoodBye() {
+	function youSayGoodBye() {
 
-// 		alert("Good Bye ..!!");
+		alert("Good Bye ..!!");
 
-// 		// A self containned inner function
-// 		function andISayHello() {
-// 			alert("Hello ..!!");
-// 		}
+		// A self containned inner function
+		function andISayHello() {
+			alert("Hello ..!!");
+		}
 
-// 		return andISayHello;
-// 	}
+		return andISayHello;
+	}
 
-// 	var something = youSayGoodBye();
-// 		something();
+	var something = youSayGoodBye();
+		something();
 
-// })(); 
+})(); 
 
+// i.e. 
+// Clousres get involved when the returned inner
+// function isn't self-contained.
+
+// i.e. not relies on outer function
 
 //A more realistic example
 
@@ -24,11 +29,10 @@
 
 	function stopWatch() {
 		var startTime = Date.now();
-		alert("startTime:" +  startTime);
 
 		function getDelay() {
 			var elapsedTime = Date.now() - startTime;
-			alert("elapsedTime: " + elapsedTime);
+			alert(elapsedTime);
 		}
 
 		return getDelay;
@@ -37,7 +41,6 @@
 	var timer = stopWatch();
 
 	//do something that takes some time
-
 	for (var i = 0; i < 1000000; i++) {
 		var foo = Math.random() *10000;
 	}
@@ -67,3 +70,28 @@
 // 				another function
 // i.e. outer context = variables this function relies on
 
+
+(function(window){
+
+    var foo = 'Hello';
+    var bar = 'World!';
+
+    function baz(){
+        return foo +' '+ bar;
+    }
+
+    window.baz = baz;
+
+})(window);
+
+// The last three lines throw exceptions because
+// currently nothing is accessible outside the 
+// anonymous function. To allow access to a variable or 
+// function, we need to expose it to the
+// global ‘window’ object.
+
+console.log(baz());
+
+//It's important to note that these still won't work: 
+console.log(foo);
+console.log(bar);
