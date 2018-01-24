889e9199697ce7f34e30c07a56ff5bda8c692df6
HimanshuP90 <himanshup895@gmail.com>
2018-01-24 15:36:36 +0530

    added Number guessing game

---
+++ Concepts/Interpreted_versus_compiled.js
@@ -0,0 +1,25 @@
+___________________________________
+Interpreted versus compiled code
+___________________________________
+
+JavaScript is an interpreted language - the code is run from top to bottom and the result of running  the code is immediately returned.
+You don't have to transform the code into a different from before the browser runs it.
+
+Compiled languages on the other hand are transformed(compiled) into another form before they run by the computer. i.e. C/C++ ..etc
+
+
+___________________________________
+Server-side versus client-side code
+___________________________________
+
+Client-side code that is run on the user's computer : JavaScript client-side 
+Server-side code that is run on the server: Node.js 
+
+
+___________________________________
+Thinking like a programmer
+___________________________________
+
+One of the hardest things to learn in programming is not the syntax you need to learn, but how to apply it to solve real world problems. You need to start thinking like a programmer — this generally involves looking at descriptions of what your program needs to do and working out what code features are needed to achieve those things, and how to make them work together.
+
+This requires a mixture of hard work, experience with the programming syntax, and practice — and a bit of creativity. The more you code, the better you'll get at it. We can't promise that you'll develop "programmer brain" in 5 minutes, but we will give you plenty of opportunity to practice thinking like a programmer throughout the course
---
+++ Concepts/Prototypes.js
@@ -0,0 +1,37 @@
+//Every JavaScript object has a prototype. The prototype is also an object.
+
+
+function foo() {}
+
+undefined
+
+foo
+
+foo()
+arguments: null
+caller: null
+length: 0
+name: "foo"
+prototype: Object { … }
+__proto__: function ()
+
+
+foo.prototype
+
+{…}
+constructor: function foo()
+__proto__: {…}
+__defineGetter__: function __defineGetter__()
+__defineSetter__: function __defineSetter__()
+__lookupGetter__: function __lookupGetter__()
+__lookupSetter__: function __lookupSetter__()
+constructor: function Object()
+hasOwnProperty: function hasOwnProperty()
+isPrototypeOf: function isPrototypeOf()
+propertyIsEnumerable: function propertyIsEnumerable()
+toLocaleString: function toLocaleString()
+toSource: function toSource()
+toString: function toString()
+unwatch: function unwatch()
+valueOf: function valueOf()
+watch: function watch()
